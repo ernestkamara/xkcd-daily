6598e4151b9a9a9bc60a7e59a9819aed1a3e022a
Ernest Saidu Kamara <ernes.kamara@evry.com>
2019-06-09 21:56:26 +0200

    Update bottom navigation items and listener

--- app/src/main/java/io/kamara/xkcd/daily/MainActivity.kt
+++ app/src/main/java/io/kamara/xkcd/daily/MainActivity.kt
@@ -3,35 +3,48 @@ package io.kamara.xkcd.daily
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
+import io.kamara.xkcd.daily.utils.Constants.Companion.PARAM_NAVIGATION_ID
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
+        loadFragment(item.itemId)
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        textMessage = findViewById(R.id.message)
+        val navView: BottomNavigationView = findViewById(R.id.navView)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        val navigationId = intent.getIntExtra(PARAM_NAVIGATION_ID, R.id.navigation_home)
+        navView.selectedItemId = navigationId
+    }
+
+
+    private fun loadFragment(itemId: Int) {
+        val tag = itemId.toString()
+        val fragment = supportFragmentManager.findFragmentByTag(tag) ?: when (itemId) {
+            R.id.navigation_home -> {
+                ComicDetailFragment.newInstance("1")
+            }
+            R.id.navigation_search -> {
+                ComicDetailFragment.newInstance("2")
+            }
+            R.id.navigation_favorites -> {
+                ComicDetailFragment.newInstance("14")
+            }
+            else -> {
+                null
+            }
+        }
+
+        // replace fragment
+        if (fragment != null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment, tag)
+                .commit()
+        }
     }
 }
